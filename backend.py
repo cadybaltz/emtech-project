c4cb37901b5e603d116195755f19b2137bd4bdb8
cadybaltz <55221077+cadybaltz@users.noreply.github.com>
2019-10-19 21:15:40 -0500

    Update backend.py
    Integrated backend with frontend

--- backend.py
+++ backend.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask
 # from app import app
 
 import speech_recognition as sr
@@ -18,15 +17,16 @@ import random
 
 import time
 
+import os
+
 
 
 all_tweets = []
-url = ['https://twitter.com/cnni', "https://twitter.com/TheEconomist", "https://twitter.com/guardiannews","https://twitter.com/washingtonposts","https://twitter.com/TheStalwart", "https://twitter.com/VitalikButerin, https://twitter.com/cnnbrk"]
+url = ['https://twitter.com/cnni']
 
 
 
-app = Flask(__name__)
-@app.route('/')
+
 
 def scanner():
     counter = 1
@@ -54,7 +54,54 @@ def scanner():
                 counter +=1
                 total +=magnitude
         results.append(total/counter)
-        return str(results[0]) + " " + str(results[1]) + " " + str(results[2]) + " "+str(results[3]) + " " + str(results[4]) + " "
+        
+    filename = """<head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="theme-color" content="#000000" />
+    
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+    <link
+      rel="apple-touch-icon"
+      sizes="76x76"
+      href="%PUBLIC_URL%/apple-icon.png"
+    />
+    
 
-if __name__=="__main__":
-    app.run(debug=True)
+    <!--     Fonts and icons     -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+    />
+
+    <title>""" + str(results[0]) + """</title>
+  </head>
+  <body>
+    <noscript> You need to enable JavaScript to run this app. </noscript>
+    <div id="root"></div>
+    <!--
+            This HTML file is a template.
+            If you open it directly in the browser, you will see an empty page.
+
+            You can add webfonts, meta tags, or analytics to this file.
+            The build step will place the bundled scripts into the <body> tag.
+
+            To begin the development, run `npm start` or `yarn start`.
+            To create a production bundle, use `npm run build` or `yarn build`.
+        -->
+  </body>"""
+
+    f=open("index.html", "w+")
+    f.write(filename)
+    f.close()
+    os.system('npm start')
+
+scanner()
